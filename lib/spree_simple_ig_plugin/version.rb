5f26ead34b37187eb00ca555cbe9e0d791d9dd40
Joseph-Reece <ndirashjose@gmail.com>
2021-12-16 14:36:18 +0300

    Init

---
+++ lib/spree_simple_ig_plugin/version.rb
@@ -0,0 +1,11 @@
+module SpreeSimpleIgPlugin
+  VERSION = '0.0.1'.freeze
+
+  module_function
+
+  # Returns the version of the currently loaded SpreeSimpleIgPlugin as a
+  # <tt>Gem::Version</tt>.
+  def version
+    Gem::Version.new VERSION
+  end
+end
